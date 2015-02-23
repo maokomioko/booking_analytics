d42d7d138387fe0c573543da6ee000f52767094d
sigra <sigra.yandex@gmail.com>
2015-02-24 01:14:31 +0400

    deploy tool

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,14 @@
-role :app, %w{maoko@ba-dev.imarto.com}
-role :web, %w{maoko@ba-dev.imarto.com}
-role :db,  %w{maoko@ba-dev.imarto.com}
+role :app, %w{mikeeirih@81.25.161.2}
+role :web, %w{mikeeirih@81.25.161.2}
+role :db,  %w{mikeeirih@81.25.161.2}
 
-server 'ba-dev.imarto.com', user: 'maoko', roles: %w{web app}
+server '81.25.161.2', user: 'mikeeirih', roles: %w{web app}
 
 set :rails_env, "staging"
+set :rvm_ruby_version, "rbx-2.5.2@booking_staging"
+
+set :branch, '1_pg'
+set :ssh_options, {
+                    forward_agent: true,
+                    port: 904
+                }

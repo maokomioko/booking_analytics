7ab2922654ac7385808443b59184935d095ecb80
sigra <sigra.yandex@gmail.com>
2015-02-24 12:20:18 +0400

    temporary disabling recurrence for parser

--- app/workers/block_availability_worker.rb
+++ app/workers/block_availability_worker.rb
@@ -4,10 +4,10 @@ class BlockAvailabilityWorker
 
   sidekiq_options retry: false, unique: true
 
-  recurrence { daily.hour_of_day(1, 5, 9, 13, 17, 21) } # UTC
+  # recurrence { daily.hour_of_day(1, 5, 9, 13, 17, 21) } # UTC
 
   def perform
-    BlockAvailability.find_in_batches(batch_size: 100) do |batch|
+    BlockAvailability.find_in_batches(batch_size: 1000) do |batch|
       BlockAvailabilityBatchWorker.perform_async batch.map(&:id)
     end
   end

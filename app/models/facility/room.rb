d109b9994ffec02cd3891f073127dd3d6c0face9
Mike Eirih <mikeeirih@me.com>
2015-03-10 04:30:08 +0800

    facility::room upd

--- app/models/facility/room.rb
+++ app/models/facility/room.rb
@@ -1,3 +1,9 @@
 class Facility::Room < ActiveRecord::Base
   include FacilityMethods
+
+  has_and_belongs_to_many :rooms,
+                          join_table: 'room_facilities_rooms',
+                          class_name: '::Room',
+                          foreign_key: 'room_facility_id',
+                          association_foreign_key: 'room_id'
 end

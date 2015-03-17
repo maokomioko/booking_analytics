b97c0d1d67920718538e7851dac18743f8704b38
Mike Eirih <mikeeirih@me.com>
2015-03-17 20:30:02 +0300

    block_prices method

--- app/models/block_availability.rb
+++ app/models/block_availability.rb
@@ -3,18 +3,19 @@ class BlockAvailability < ActiveRecord::Base
 
   scope :for_hotels, -> (hotel_ids){ where("(data->>'hotel_id')::integer IN (?)", hotel_ids) }
   scope :with_occupancy, -> (occupancy){ where("jsonb2arr((data->'block'), 'max_occupancy') @> '{\"?\"}'::text[]", occupancy) }
+
   scope :by_arrival, -> (date){ where("(data->>'arrival_date') = ?", date.to_date.strftime("%Y-%m-%d")) }
   scope :by_departure, -> (date){ where("(data->>'departure_date') = ?", date.to_date.strftime("%Y-%m-%d")) }
-  
-  def block_prices(occupancy)
-    blocks = data['block'].select {|block| block['max_occupancy'] == occupancy}
 
-    names = []
+  def block_prices(occupancy)
+    blocks = data['block'].select { |block| block['max_occupancy'] == occupancy.to_s }
+
+    arr = []
     blocks.each do |block|
-      names << block.fetch('incremental_price')
+      arr << block.fetch('incremental_price').map{|x| x['price']}.sort.first
     end
 
-    names
+    arr
   end
 
   class << self

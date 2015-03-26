ee526b491918901673bb3262cb351e6ba2df942c
sigra <sigra.yandex@gmail.com>
2015-03-26 13:12:55 +0400

    add manual option to PriceWorker

--- price_maker/app/workers/price_maker/price_worker.rb
+++ price_maker/app/workers/price_maker/price_worker.rb
@@ -8,16 +8,18 @@ module PriceMaker
       @expiration ||= 60*60*24*5 # 5 days
     end
 
-    def perform(hotel_id)
+    def perform(hotel_id, manual = false)
       hotel = Hotel.find(hotel_id)
 
       hotel.rooms.real.each do |room|
         room.fill_prices if room.roomtype?
       end
 
-      time = hotel.channel_manager.company.setting.crawling_frequency rescue Setting.default_attributes[:crawling_requency]
-      job_id = PriceMaker::PriceWorker.perform_in(time, hotel_id)
-      hotel.update_column(:current_job, job_id)
+      unless manual
+        time = hotel.channel_manager.company.setting.crawling_frequency rescue Setting.default_attributes[:crawling_requency]
+        job_id = PriceMaker::PriceWorker.perform_in(time, hotel_id)
+        hotel.update_column(:current_job, job_id)
+      end
     end
   end
 end

00e308d4f81ed74a940a35f23f7292f1e5a02963
Mike Eirih <mikeeirih@me.com>
2015-04-19 21:43:12 +0200

    css/views updated to match new js

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,10 @@
 class InvitationsController < Devise::InvitationsController
   before_filter :check_ability, only: [:new, :create]
 
+  def new
+    redirect_to company_users_path
+  end
+
   def create
     emails = invite_params[:email].split(',')
     errors  = []
@@ -42,17 +46,6 @@ class InvitationsController < Devise::InvitationsController
   end
 
   def invite_resource(email = nil, &block)
-    # @user = User.find_by(email: email)
-    # # @user is an instance or nil
-    # if @user && @user.email != current_user.email
-    #   # invite! instance method returns a Mail::Message instance
-    #   @user.invite!(current_user)
-    #   # return the user instance to match expected return type
-    #   @user
-    # else
-    #   # invite! class method returns invitable var, which is a User instance
-    #   resource_class.invite!({ email: email, role: 'client' }, current_inviter, &block)
-    # end
     resource_class.invite!({ email: email, role: 'client' }, current_inviter, &block)
   end
 end
